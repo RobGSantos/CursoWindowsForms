9ec330d94647e2d78259f929c0c8c798b18458e9
Robson Gonçalves Santos <robsongsantos@PREFEITURA.SP.GOV.BR>
2026-02-10 10:11:08 -0300

    Teste_01

---
+++ CursoWindowsForms/Frm_HellowWorld.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_HellowWorld : Form
+    {
+        public Frm_HellowWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Btn_ModificalLabel_Click(object sender, EventArgs e)
+        {
+            Lbl_Titulo.Text = Txt_ConteudoLabel.Text;
+
+        }
+    }
+}
