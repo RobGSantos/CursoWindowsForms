52f7cc8eb7e01a96a5c274842457fe2240f48010
Robson Gonçalves Santos <robsongs@yahoo.com>
2026-02-11 14:28:06 -0300

    Commit ALL

--- CursoWindowsForms/Frm_ValidaCPF2.cs
+++ CursoWindowsForms/Frm_ValidaCPF2.cs
@@ -16,11 +16,13 @@ namespace CursoWindowsForms
         public Frm_ValidaCPF2()
         {
             InitializeComponent();
+            Msk_CPF.Focus();
         }
 
         private void Btn_Reset_Click(object sender, EventArgs e)
         {
             Msk_CPF.Text = string.Empty;
+            Msk_CPF.Focus();
         }
 
         private void Btn_Valida_Click(object sender, EventArgs e)
@@ -31,30 +33,36 @@ namespace CursoWindowsForms
             if (string.IsNullOrEmpty(vConteudo))
             {
                 MessageBox.Show("O campo CPF está vazio! Digite o valor do CPF!", "CAMPO CPF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Msk_CPF.Focus();
                 return;
             }
 
             if(vConteudo.Length < 11)
             {
                 MessageBox.Show("O CPF digitado não contém 11 números! Digite os 11 números do CPF!", "CAMPO CPF", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Msk_CPF.Focus();
                 return;
             }
 
-            if(MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação",MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if(MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação",MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
             {
-                bool validaCPF = false;
-                validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
-                if (validaCPF)
-                {
-                    MessageBox.Show("CPF válido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("CPF inválido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                Msk_CPF.Focus();
+                return;
             }
 
-            
+            Msk_CPF.Focus();
+
+            bool validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
+
+            if (validaCPF)
+            {
+                MessageBox.Show("CPF válido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("CPF inválido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
